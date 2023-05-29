127b3b7e86108996a0d903953c75f6165c965bac
Michal-Kret <michal.kret.stud@pw.edu.pl>
2023-05-29 23:26:21 +0200

    Dodałem .yaml dla utleniacza i paliwa, oraz główny skrypt i funkcję składu materiału pędnego

---
+++ PropellantComposition.m
@@ -0,0 +1,7 @@
+function propellant=PropellantComposition()
+%PropellantComposition() - function used to define compounds and mass
+%ratios of analyzed propellant
+%Michał Kret MKWS 2023
+propellant=Solution('AP.yaml');
+end
+
